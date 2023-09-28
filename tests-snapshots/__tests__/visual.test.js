bfb9df370d551b4609b1e3f360e7a2d3eadafe7d
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-09-28 20:56:39 +0900

    Visual Regression Testing with Jest

---
+++ tests-snapshots/__tests__/visual.test.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer')
+const { toMatchImageSnapshot } = require('jest-image-snapshot')
+
+expect.extend({ toMatchImageSnapshot })
+
+describe('Visual Regression Testing', () => {
+  let browser
+  let page
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: `new` })
+    page =  await browser.newPage()
+  })
+
+  afterAll(async () => {
+    await browser.close()
+  })
+
+  test('Full Page Snapshot', async () => {
+    await page.goto('https://www.example.com')
+    await page.waitForSelector('h1')
+    const image = await page.screenshot()
+    expect(image).toMatchImageSnapshot({
+      failureThresholdType: "pixel",
+      failureThreshold: 500
+    })
+  })
+
+  test('Single Element Snapshot', async () => {
+    await page.goto('https://www.example.com')
+    const h1 = await page.waitForSelector('h1')
+    const image = await h1.screenshot()
+    expect(image).toMatchImageSnapshot({
+      failureThresholdType: "percent",
+      failureThreshold: 0.01
+    })
+  })
+})
